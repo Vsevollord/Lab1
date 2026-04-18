23785a3b7face798a6744d7d7dffc63b2ea29289
Vsevollord <Vsevollords@gmail.com>
2026-04-18 11:45:14 +0300

    task2_fix

--- task2.py
+++ task2.py
@@ -1,13 +1,16 @@
 from functions import *
 
-def second_max(a):
-  max1=max2=a[0]
-  for i in a:
-    if i>max1:
-      max2=max1
-      max1=i
 
-  return max2
+def second_max(a):
+    max1 = max2 = float('-inf')
+    for i in a:
+        if i > max1:
+            max2 = max1
+            max1 = i
+        elif i > max2 and i != max1:
+            max2 = i
+
+    return max2
 
 if __name__ == '__main__':
     sizes=[100, 1000, 5000, 10000]
